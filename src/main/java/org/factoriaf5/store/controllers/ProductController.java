eddbfa63fd2f1abf22b04e4f25e87478a41fa70a
anagarcialainez <accgl@hotmail.com>
2021-05-17 15:25:06 +0200

    agregando clases

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,14 +1,29 @@
 package org.factoriaf5.store.controllers;
 
+import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ProductController {
-     @GetMapping("/health")
+    @GetMapping("/health")
     public String check() {
         return "Hello! I'm a server";
+    }
 
+    public List<Product> products = new ArrayList<>();
+
+    @GetMapping("/products")
+    public List<Product> allProducts() {
+        return products;
     }
 }
+
+    /*@PostMapping("/products")
+    public RedirectView addNewProduct(Product product) {
+        products.add(product);
+        return new RedirectView("/"); */
 
